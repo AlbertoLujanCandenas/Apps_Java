db968f2670c929771addf755d14f4099dc8e2a8b
Alberto Luján <93909889+AlbertoLujanCandenas@users.noreply.github.com>
2022-10-19 18:43:39 +0200

    Final version

---
+++ Springboot/Metro/src/main/java/Repositorio/LineaRepositorio.java
@@ -0,0 +1,8 @@
+package Repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+
+import Model.Linea;
+
+public interface LineaRepositorio extends CrudRepository<Linea, Integer>{
+}
